283b2a20cfde68fa567d06928c7cc6cedd87981a
Ankan Misra <ankan-macbook@Ankans-MacBook-Air.local>
2025-03-05 20:29:33 +0530

    .

--- frontend/src/components/landing/LandingPage.tsx
+++ frontend/src/components/landing/LandingPage.tsx
@@ -424,6 +424,8 @@ const LandingPage = ({ connected }: LandingPageProps) => {
               </SignInButton>
             </SignedOut>
             <SignedIn>
+
+              
               <motion.button
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
