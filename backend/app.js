5a3e23caadf0074f004abb4061d94db2b6d51f6b
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-03-10 01:08:40 +0530

    Add a custom signup route in the backend

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config({path:"./config.env"}) // Setting the config.env file
 
 const app=express()
 
-app.use(express.json()) //It is used to read the json file that we get from the request
+app.use(express.json()) //It is used to read the json file that we get from the Request
 
 app.use('/api/v1/user',router)
 
